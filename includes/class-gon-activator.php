65ff38af711b55725770e0c74b93636b9f0ca512
Johan Hallberg <johan.hallberg@gmail.com>
2021-02-24 13:11:29 +0100

    Renamed

---
+++ includes/class-gon-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       http://example.com
+ * @since      1.0.0
+ *
+ * @package    Gon
+ * @subpackage Gon/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Gon
+ * @subpackage Gon/includes
+ * @author     Your Name <email@example.com>
+ */
+class Gon_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
